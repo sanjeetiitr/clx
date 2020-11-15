1495422cf08097fe69dae1cb564891d823f77ad0
Sanjeet-Office <sanjeet-office@Sanjeet-MacBook-Pro.local>
2020-11-15 18:27:21 +0530

    Test component removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Suspense } from "react";
-import { Route, BrowserRouter, Router, Switch, withRouter } from "react-router-dom";
+
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { CanvasPlaygroundContainer } from "./Container/CanvasPlayground";
 import { MainPage } from "./Container/MainPage";
@@ -8,7 +8,6 @@ const customHistory = createBrowserHistory();
 
 export default function App() {
   return (
-
     <BrowserRouter fallback={<div>Suspence</div>}>
       <Switch history={customHistory}>
         <Route exact path={"/"} component={(props) => <MainPage {...props} />} />
